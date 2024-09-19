fe4b348c316298f7576e6c2e17467259539d7d49
kevmateo <117870690+kevmateo@users.noreply.github.com>
2024-09-19 17:50:07 +0000

    testeando

--- Codigo/src/componentes/Moleculas/TarjetaPrestamos.jsx
+++ Codigo/src/componentes/Moleculas/TarjetaPrestamos.jsx
@@ -1,22 +1,23 @@
 
 function TarjetaPrestamo({ nombreCliente, cedulaCliente, cuotasRestantes, saldoPendiente, onClick }) {
 
-  return(
-
-    <div className="h-[136px] w-[307px] p-[15px] flex flex-col rounded-[10px] bg-white shadow-3xl cursor-pointer" onClick={onClick}>
-      <span className="font-bold text-AzulSlide text-2xl truncate">{nombreCliente}</span>
-      <span>{cedulaCliente}</span>
-      <section className="mt-[10px] flex justify-between">
-        <div className="flex flex-col">
-          <span className="text-sm">Cuotas Restantes</span>
-          <span className="font-bold">{cuotasRestantes} Meses</span>
-        </div>
-        <div className="flex flex-col">
-          <span className="text-sm">Saldo Pendiente</span>
-          <span className="font-bold">$ {saldoPendiente}</span> 
-        </div>
-      </section>
-    </div>
+  return (
+    <Suspense fallback={<TarjetaPrestamoGlimmer />}>
+      <div className="h-[136px] w-[307px] p-[15px] flex flex-col rounded-[10px] bg-white shadow-3xl cursor-pointer" onClick={onClick}>
+        <span className="font-bold text-AzulSlide text-2xl truncate">{nombreCliente}</span>
+        <span>{cedulaCliente}</span>
+        <section className="mt-[10px] flex justify-between">
+          <div className="flex flex-col">
+            <span className="text-sm">Cuotas Restantes</span>
+            <span className="font-bold">{cuotasRestantes} Meses</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-sm">Saldo Pendiente</span>
+            <span className="font-bold">$ {saldoPendiente}</span>
+          </div>
+        </section>
+      </div>
+    </Suspense>
   );
 }
 
