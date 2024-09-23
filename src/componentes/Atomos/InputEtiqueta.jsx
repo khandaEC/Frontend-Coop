73132310f2f504be3c24aa3dc19f0272e2cead88
kevmateo <117870690+kevmateo@users.noreply.github.com>
2024-09-23 18:12:08 +0000

    boton en el input

--- src/componentes/Atomos/InputEtiqueta.jsx
+++ src/componentes/Atomos/InputEtiqueta.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 
-function InputEtiqueta({ etiqueta, type, placeholder, value, onChange, width, requerido, forceValidate, onKeyDown }) {
+function InputEtiqueta({ etiqueta, type, placeholder, value, onChange, width, requerido, forceValidate, textoBoton, onClick, colorBoton, onKeyDown, onKeyDownBoton, boton }) {
   const [error, setError] = useState('');
+  const [boton, setBoton] = useState(false);
 
   const handleBlur = () => {
     if (requerido && !value) {
@@ -14,26 +15,27 @@ function InputEtiqueta({ etiqueta, type, placeholder, value, onChange, width, re
 
   useEffect(() => {
     if (forceValidate) {
-      handleBlur(); // Forzar la validación en el blur cuando se haga clic en "Siguiente"
+      handleBlur();
     }
   }, [forceValidate]);
 
   return (
-    <div className="flex flex-col">
-      <span className="font-bold">{etiqueta}</span>
-      <input
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        onBlur={handleBlur}
-        onWheel={(e) => e.target.blur()}
-        onKeyDown={onKeyDown}
-        className={`h-[45px] rounded-[10px] border px-[10px] focus:outline-gray-300 ${error ? 'border-red-500' : 'border-Gris'}`}
-        style={{ width: width }}
-      />
-      {error && <span className="text-xs text-red-500">{error}</span>}
-      <style>{`
+    <>
+      <div className="flex flex-col">
+        <span className="font-bold">{etiqueta}</span>
+        <input
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          onBlur={handleBlur}
+          onWheel={(e) => e.target.blur()}
+          onKeyDown={onKeyDown}
+          className={`h-[45px] rounded-[10px] border px-[10px] focus:outline-gray-300 ${error ? 'border-red-500' : 'border-Gris'}`}
+          style={{ width: width, borderRadius: boton ? '10px 10px 0 0' : '10px' }}
+        />
+        {error && <span className="text-xs text-red-500">{error}</span>}
+        <style>{`
         input[type=number]::-webkit-inner-spin-button,
         input[type=number]::-webkit-outer-spin-button {
           -webkit-appearance: none;
@@ -43,7 +45,16 @@ function InputEtiqueta({ etiqueta, type, placeholder, value, onChange, width, re
           -moz-appearance: textfield;
         }
       `}</style>
-    </div>
+      </div>
+      <button 
+        onClick={onClick}
+        onKeyDown={onKeyDownBoton}
+        className={`rounded-[10px] px-[20px] py-[10px] text-white font-bold shadow-3xl`}
+        style={{ backgroundColor: colorBoton, display: boton ? 'block' : 'none', borderRadius: '0 0 10px 10px' }}
+      >
+      {textoBoton}
+      </button>
+    </>
   );
 }
 
