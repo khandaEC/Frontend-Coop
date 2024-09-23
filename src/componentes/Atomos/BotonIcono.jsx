4c767e12e04ab202715ef1b65ecc97d041bfe1ac
kevmateo <kevin.cano@epn.edu.ec>
2024-09-23 07:05:44 -0500

    rama acutalizada

--- src/componentes/Atomos/BotonIcono.jsx
+++ src/componentes/Atomos/BotonIcono.jsx
@@ -3,11 +3,11 @@
 function BotonIcono({ iconoDerecha, iconoIzquierda, texto, onClick, width }) {
   return(
     <button 
-      className="flex items-center justify-center h-[44px] bg-transparent text-[#5A6268] font-bold "
+      className="flex items-center justify-center h-[44px] bg-transparent text-[#5A6268] font-bold"
       style={{ width: width }}
       onClick={onClick}
     >
-      <span className="pr-1">{iconoIzquierda}</span>
+      <span className="pr-3 border">{iconoIzquierda}</span>
       <span>{texto}</span>
       <span className="pl-3">{iconoDerecha}</span>
     </button>
